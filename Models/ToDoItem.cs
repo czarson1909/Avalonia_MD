84e9a09c2b30604b11b852ff1408e2faa9dfc916
Cezary Kur <109467719+czarson1909@users.noreply.github.com>
2024-07-05 17:47:50 +0200

    Add files via upload

---
+++ Models/ToDoItem.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SimpleToDoList.Models;
+
+
+public class ToDoItem
+{
+
+    public bool IsChecked { get; set; }
+
+
+    public string? Content { get; set; }
+}
